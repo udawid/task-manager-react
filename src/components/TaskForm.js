ffe61cd92da11394fe2589904384859737d722d7
Dawid Urbaniak <udawid00@gmail.com>
2023-12-11 10:50:20 +0100

    Add styles and TaskForm

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const TaskForm = () => {
-  return <div></div>;
+  return (
+    <form className="TaskForm">
+      <input type="text" className="task-input" placeholder="Add a task" />
+      <button type="submit" className="task-btn"></button>
+    </form>
+  );
 };
 
 export default TaskForm;
